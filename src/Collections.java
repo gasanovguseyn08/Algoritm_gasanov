96009e164970a5be0177457ff93f182036c7a827
gasanovguseyn08 <gusiein.gasanov81@gmail.com>
2025-01-29 21:40:42 +0300

    Update Collections.java

--- src/Collections.java
+++ src/Collections.java
@@ -3,24 +3,28 @@ import java.util.List;
 
 public class Collections {
 
+    // Бинарный поиск в списке с Comparable
     public static <T extends Comparable<? super T>> int binarySearch(List<T> list, T key) {
-        return binarySearch(list, key, Comparator.naturalOrder());
+        Comparator<T> comparator = Comparator.naturalOrder();
+        return binarySearch(list, key, comparator);
     }
 
-    // Binary search for Lists with a Comparator
-    public static <T> int binarySearch(List<T> list, T key, Comparator<? super T> c) {
-        int fromIndex = 0, toIndex = list.size();
-        while (fromIndex < toIndex) {
-            int mid = (fromIndex + toIndex) >>> 1;
-            int cmp = c.compare(list.get(mid), key);
-            if (cmp < 0) {
-                fromIndex = mid + 1;
-            } else if (cmp > 0) {
-                toIndex = mid;
+    // Бинарный поиск в списке с заданным компаратором
+    public static <T> int binarySearch(List<T> list, T key, Comparator<? super T> comparator) {
+        int start = 0, end = list.size();
+        
+        while (start < end) {
+            int middle = (start + end) >>> 1;
+            int comparison = comparator.compare(list.get(middle), key);
+            
+            if (comparison < 0) {
+                start = middle + 1;
+            } else if (comparison > 0) {
+                end = middle;
             } else {
-                return mid;
+                return middle;
             }
         }
-        return -(fromIndex + 1);
+        return -(start + 1);
     }
 }
